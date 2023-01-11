161f2ed4fdcb18a514e4df7b61102581a5b4d69f
Рамиль <ramasafiullin@yandex.ru>
2023-01-11 14:30:32 +0300

    UpdateTask

---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+
+    private String message;
+
+    public Cat(){
+
+    }
+
+    public String getName() {
+        return message;
+    }
+
+    public void setName(String name) {
+        this.message = message;
+    }
+}
